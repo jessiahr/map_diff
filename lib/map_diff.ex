cbc1c03056108bbc79e014c92dbab39d165cff28
Jessiah Ratliff <jessiahr@gmail.com>
2017-08-25 11:26:12 -0700

    init

---
+++ lib/map_diff.ex
@@ -0,0 +1,18 @@
+defmodule MapDiff do
+  @moduledoc """
+  Documentation for MapDiff.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapDiff.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
